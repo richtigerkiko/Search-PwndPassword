e0455e8e5682f3bcc088829d18bd35220aed745a
elkikos@gmail.com <a.kiko@redhouse.de>
2019-09-04 16:42:37 +0200

    Initial Commit

--- Readme.md
+++ Readme.md
@@ -1,2 +1,28 @@
 # Search-PwndPassword
 
+## Description
+
+This Script checks if the given string is in the password database of https://haveibeenpwned.com/Passwords
+
+It uses K-Anonymity and works like this: 
+ -  The script hashes the password
+ -  the hash is memorized and then shorten to the first 5 characters
+ -  the 5 characters is send to the api (https://api.pwnedpasswords.com/range/)
+ -  the api gives back a list with complete hashes that start with the 5 characters
+ -  we check if the list contains the memorized full hash.
+
+
+## Example
+
+```
+PS C:\Temp\source\Powershell\Search-PwndPassword> Import-Module .\Search-PwndPassword.ps1
+PS C:\Temp\source\Powershell\Search-PwndPassword> Pony1 | Search-PwndPassword
+PS C:\Temp\source\Powershell\Search-PwndPassword> "Pony1" | Search-PwndPassword
+WARNUNG: Password Found: 19
+
+
+ Times
+PS C:\Temp\source\Powershell\Search-PwndPassword> "LOLPower" | Search-PwndPassword
+Password not found.
+PS C:\Temp\source\Powershell\Search-PwndPassword>
+```
---
+++ Search-PwndPassword.ps1
@@ -0,0 +1,85 @@
+#Requires -Version 3
+
+Function Search-PwndPassword{
+<#
+.SYNOPSIS
+    Searches a password database
+.DESCRIPTION
+    This Function Checks the Given Password with HaveIBeenPwnd Database using K-Anonymity
+
+.PARAMETER Password
+    Cleartext Password. Value can be piped. If Parameter is not Set a windows Credential box will appear
+
+.NOTES
+
+  Version:        1.0
+
+  Author:         Alexander Kiko
+
+  Creation Date:  26.04.2019
+
+  Purpose/Change: Initial script devpelopment
+  
+
+.EXAMPLE
+
+  Get-PwndPassword
+
+  "Pony1" | Get-PwndPassword
+
+  Get-PwndPassword "Pony1"
+
+#>
+
+[cmdletbinding()]
+
+Param (
+
+[parameter(ValueFromPipeline)]
+[string]
+$Password
+)
+
+
+    Begin{
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $baseUrlOfService = "https://api.pwnedpasswords.com/range/"
+    
+        Function Get-StringHash([String] $String,$HashName = "SHA1") 
+        {    
+            $StringBuilder = New-Object System.Text.StringBuilder 
+            [System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String)) | ForEach-Object{ 
+                [Void]$StringBuilder.Append($_.ToString("x2")) 
+            } 
+            $StringBuilder.ToString() 
+        }
+    }
+    
+    Process{
+    
+        if($Password){
+            $hashWord = Get-StringHash $Password
+        }
+        else{
+            $hashWord = Get-StringHash (Get-Credential -UserName "NOT IMPORTANT" -Message "not important").GetNetworkCredential().Password
+        }
+    
+        $shortHash = $hashWord.SubString(0,5)
+    
+        $hashSuffix = $hashWord.SubString(5,35) + ":"
+    
+        
+        $resultList = Invoke-WebRequest -Uri ($baseUrlOfService + $shortHash) | Select-Object Content -ExpandProperty Content
+    
+        $result = $resultList.Split('') | Select-String $hashSuffix | Out-String
+    
+        if($result){
+            $count = $result.Split(":")[1]
+            Write-Warning "Password Found: $count Times"
+        }
+        else{
+            Write-Output  'Password not found.'
+        }
+        
+    }
+}
